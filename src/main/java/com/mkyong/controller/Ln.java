8075ce979b92de738fedb2ed2fe37592dc343ccb
aithentgit <rama.kumawat.16@gmail.com>
2017-02-16 11:36:31 +0530

    commiting in the branch test1b1

---
+++ src/main/java/com/mkyong/controller/Ln.java
@@ -0,0 +1,9 @@
+package com.mkyong.controller;
+public class Ln{
+ public static void main(String ar[]){
+   System.out.println("how are you man");
+ }
+ public void m1(){
+	 System.out.println("sldk");
+ }
+}
